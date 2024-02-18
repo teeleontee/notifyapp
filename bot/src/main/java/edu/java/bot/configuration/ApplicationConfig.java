5827a9b344d03abd089c6e35a1007fd396575f80
teeleontee <leontyevts@gmail.com>
2024-02-18 21:46:26 +0300

    properties

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -3,12 +3,14 @@ package edu.java.bot.configuration;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 @Data
 @Validated
 @Configuration
+@ComponentScan
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public class ApplicationConfig {
     @Value("${token}")
