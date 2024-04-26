b0879cb40bbaa0e615120487eacb1dbb822594ec
teeleontee <leontyevts@gmail.com>
2024-04-26 20:47:10 +0300

    scrapper-jooq

---
+++ scrapper-jooq/src/test/java/org/example/scrapperjooq/ScrapperJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scrapperjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
